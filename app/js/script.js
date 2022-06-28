1bbe70840ab9de3c4bfddcd46539eef318903d5b
VKhvesyk <khvesyk.vasyl@gmail.com>
2022-06-28 19:20:43 +0300

    Created js code for slider. Added some styles for news and header sections

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
 "use strict"
 const menuBtn = document.querySelector('.header-menu__button');
 const menu = document.querySelector('.header-menu__wrapper');
@@ -20,8 +21,8 @@ function toggleMenu() {
 
 // Search
 const logo = document.querySelector('.header-logo');
-// const searchInput = document.querySelector('');
-const searchBtn = document.querySelector('.header-search__icon');
+// const search = document.querySelector('.header-search');
+const searchBtn = document.querySelector('.header-search__input');
 
 searchBtn.addEventListener('click', function(event) {
     event.preventDefault();
@@ -29,9 +30,13 @@ searchBtn.addEventListener('click', function(event) {
 });
 
 function showSearch() {
-    logo.classList.toggle('header-logo_hide');
-    // seachBtn.classList.toggle('_active');
+    logo.classList.toggle('header-logo_fade');
+    search.classList.toggle('header-search_active');
     // searchInput.classList.toggle('_active');
+
+    setTimeout(() => {
+      logo.classList.toggle('header-logo_hide');
+  }, 200);
 };
 
 
@@ -85,3 +90,103 @@ let tabs = document.querySelectorAll('.betting-tabs__button'),
     
     hideTabContent();
     showTabContent(0);
+
+
+
+// Slider
+
+let offset = 0;
+
+const slides = document.querySelectorAll('.js-slider__slide'),
+      prev = document.querySelector('.js-slider__prev'),
+      next = document.querySelector('.js-slider__next'),
+      slider = document.querySelector('.js-slider'),
+      sliderWrapper = document.querySelector('.js-slider__wrapper'),
+      width = window.getComputedStyle(slider).width,
+      // slideWidth = document.querySelector('.js-slider__slide').clientWidth,
+      touchZone = document.querySelectorAll('.js-slider__slide');
+
+
+  sliderWrapper.style.width = (100 * slides.length) + '%';
+  // sliderWrapper.style.width = (slideWidth * (slides.length + 1)) + ((slides.length) * 30) + 'px';
+// sliderWrapper.style.width = 250 * (slides.length) + ((slides.length - 1) * 20) + 'px';
+
+  sliderWrapper.style.transition = '0.5s all';
+  slider.style.overflow = 'hidden';
+
+  slides.forEach(slide => {
+      slide.style.width = width;
+  });
+
+function moveToPrevSlide() {
+    if (offset == 0) {
+        offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+    } else {
+        offset -= +width.slice(0, width.length - 2);
+    }
+
+    sliderWrapper.style.transform = `translateX(-${offset}px)`;
+}
+
+function moveToNextSlide() {
+    if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
+        offset = 0;
+    } else {
+        offset += +width.slice(0, width.length - 2);
+    }
+
+    sliderWrapper.style.transform = `translateX(-${offset}px)`;
+}
+
+// next.addEventListener('click', () => {
+
+// });
+
+// prev.addEventListener('click', () => {
+
+// });
+
+// swipe
+touchZone.forEach(slide => {
+  slide.addEventListener('touchstart', handleTouchStart, {passive: true});
+});
+touchZone.forEach(slide => {
+  slide.addEventListener('touchmove', handleTouchMove, {passive: true});
+});
+
+let x1 = null;
+let y1 = null;
+
+function handleTouchStart(event) {
+  const firstTouch = event.touches[0];
+
+  x1 = firstTouch.clientX;
+  y1 = firstTouch.clientY;
+}
+
+function handleTouchMove(event) {
+  if (!x1 || !y1) {
+    return false;
+  }
+
+  let  x2 = event.touches[0].clientX;
+  let  y2 = event.touches[0].clientY;
+
+  let xDiff = x2 - x1;
+  let yDiff = y2 - y1;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    // move left - right or right - left
+    if (xDiff > 0) {
+      moveToPrevSlide();
+    } else {
+      moveToNextSlide();
+    }
+  }
+
+  x1 = null;
+  y1 = null;
+}
+
+
+});
